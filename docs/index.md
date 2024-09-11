b5b4b6bcb6ee0b14caf770c34223f06246b89cae
lksprado <lks-prado@live.com>
2024-09-11 17:25:50 -0300

    feat: fights.py webscrapping.py

---
+++ docs/index.md
@@ -0,0 +1,4 @@
+# GAME DAY RIVALRY WARNING
+
+The objective of this project is to receive an email on the days of NHL regular-season games that involve at least one of the top 50 rivalries of the past 25 years. Rivalries are defined based on the number of fights involving the teams, with data scraped from hockeyfights.com obtained from a separate project. The regular season starts in October, and a DAG will scan the game schedule and the top 50 rivalries spreadsheet. In case of a match, an email will be sent containing details of the games and their timings.
+
---
+++ src/webscrapping.py
@@ -0,0 +1,91 @@
+import datetime
+import json
+import os
+
+import requests
+from requests.adapters import HTTPAdapter
+from urllib3.util import Retry
+
+
+class NhlScrapper:
+    def __init__(self):
+        self.origin = "https://api-web.nhle.com/v1/schedule"
+        self.http = self._configure_session()
+        self.gameweek = ""
+        self.data_dir = "data/json/"
+
+    # SETTING A A REQUESTS.SESSION OBJECT WITH CUSTOM CONFIGURATION TO HANDLE HTTP REQUESTS
+    def _configure_session(self):
+        retry_strategy = Retry(
+            total=3,  # HOW MANY RETRIES
+            status_forcelist=[
+                403,
+                429,
+                500,
+                502,
+                503,
+                504,
+            ],  # FOR THESE STATUS CODE FAILURE
+        )
+        adapter = HTTPAdapter(max_retries=retry_strategy)  # CONTROLS THE REQUEST CALLS
+        session = (
+            requests.Session()
+        )  # TO PERSIST SESSION VARIABLES FOR THE FOLLOWING REQUESTS
+        session.mount("https://", adapter)
+        session.mount("http://", adapter)
+        return session
+
+    # LISTING ALL GAME WEEKS TO QUERY
+    def generate_gameweeks(self, startdate, enddate):
+        gameweeks_ls = []
+        current_date = startdate
+        while current_date <= enddate:
+            gameweeks_ls.append(current_date.strftime("%Y-%m-%d"))
+            current_date += datetime.timedelta(days=7)
+        return gameweeks_ls
+
+    def call_api(self):
+        url = f"{self.origin}/{self.gameweek}"
+        print(f"Request URL: {url}")  # Print the full URL for debugging
+        headers = {
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 Edg/128.0.0.0"
+        }
+        try:
+            response = self.http.get(url, headers=headers)
+            if response.status_code == 200:
+                return response.json()
+            else:
+                print(
+                    f"Failed to retrieve data for {self.gameweek}. Status code: {response.status_code}"
+                )
+                return None
+
+        except Exception as e:
+            print(f"Error during API call: {e}")
+            return None
+
+    def extract_data(self, games, gameweek):
+        # Salva o arquivo JSON no diretório 'data/'
+        file_path = os.path.join(self.data_dir, f"{gameweek}.json")
+        with open(file_path, "w") as json_file:
+            json.dump(games, json_file, indent=4)
+        print(f"Data for {gameweek} saved to {file_path}")
+
+
+if __name__ == "__main__":
+    # Definir datas de início e término
+    startdate = datetime.date(2024, 9, 21)
+    enddate = datetime.date(2025, 4, 12)
+
+    # Instancia o scrapper
+    scrapper = NhlScrapper()
+
+    # Gera semanas de jogo
+    gameweeks = scrapper.generate_gameweeks(startdate, enddate)
+
+    # Itera por cada gameweek, faz a chamada à API e salva os dados
+    for week in gameweeks:
+        scrapper.gameweek = week
+        games = scrapper.call_api()
+        if games:
+            scrapper.extract_data(games, week)
